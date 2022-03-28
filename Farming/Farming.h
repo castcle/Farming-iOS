2d06f08fcde4aa3621a80f0fbde88486fc6410a2
Tanakorn Phoochaliaw <tanakorn.ph@castcle.com>
2022-03-28 13:30:58 +0700

    Init project farming.

---
+++ Farming/Farming.h
@@ -0,0 +1,18 @@
+//
+//  Farming.h
+//  Farming
+//
+//  Created by Tanakorn Phoochaliaw on 28/3/2565 BE.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Farming.
+FOUNDATION_EXPORT double FarmingVersionNumber;
+
+//! Project version string for Farming.
+FOUNDATION_EXPORT const unsigned char FarmingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Farming/PublicHeader.h>
+
+
